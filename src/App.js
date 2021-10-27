90a8273d8aa3c6cb906910a59e29e27ccc60eddd
Peter Murutu <murutupeter@gmail.com>
2021-10-27 11:39:14 +0300

    compounds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Card } from './components/'
-import movies from './data.json'
+// import './App.scss'
+import movies from './data'
 
 function App() {
   return (
     <main>
       {movies.map((movie) => (
-        <Card className="mr"></Card>
+        <Card className="mr" key={`${movie.id}`}>
+          <Card.Image src={movie.image} alt={movie.title} />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Button>{movie.ctaText}</Card.Button>
+          </Card.Body>
+        </Card>
       ))}
     </main>
   )
